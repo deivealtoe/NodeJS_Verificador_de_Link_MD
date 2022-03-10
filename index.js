2104af6dc88964e4f019bea5af4792cdeadebab7
Deive Altoé <deivealtoe@gmail.com>
2022-03-09 23:55:12 -0300

    Alterado para utilizar async await

--- index.js
+++ index.js
@@ -4,23 +4,26 @@ import chalk from 'chalk'
 import fs from 'fs'
 import path from 'path'
 
-const caminhoDoArquivoMD = path.join(path.resolve(), 'files', 'texto_01.md')
+const filePath = path.join(path.resolve(), 'files', 'texto_01.md')
 
-function getFile(caminhoDoArquivoMD) {
-  // fs.readFile(caminhoDoArquivoMD, 'utf-8', (err, data) => {
-  //   if (err) showError(err)
+// function getFile(caminhoDoArquivoMD) {
+//   fs.promises
+//     .readFile(caminhoDoArquivoMD, 'utf-8')
+//     .then((data) => console.log(chalk.green(data)))
+//     .catch((err) => showError(err))
+// }
 
-  //   console.log(chalk.green(data))
-  // })
-
-  fs.promises
-    .readFile(caminhoDoArquivoMD, 'utf-8')
-    .then((data) => console.log(chalk.green(data)))
-    .catch((err) => showError(err))
+async function getFile(filePath) {
+  try {
+    const text = await fs.promises.readFile(filePath, 'utf-8')
+    console.log(chalk.green(text))
+  } catch (err) {
+    showError(err)
+  }
 }
 
 function showError(err) {
   throw new Error(chalk.red(err))
 }
 
-getFile(caminhoDoArquivoMD)
+getFile(filePath)
