a2ae69db9766a9a7e22c343aa2bc7999869e0e89
Deive Altoé <deivealtoe@gmail.com>
2022-03-11 00:03:21 -0300

    Adicionado regex

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ import path from 'path'
 
 const filePath = path.join(path.resolve(), 'files', 'texto_01.md')
 
-// function getFile(caminhoDoArquivoMD) {
-//   fs.promises
-//     .readFile(caminhoDoArquivoMD, 'utf-8')
-//     .then((data) => console.log(chalk.green(data)))
-//     .catch((err) => showError(err))
-// }
-
 async function getFile(filePath) {
   try {
     const text = await fs.promises.readFile(filePath, 'utf-8')
@@ -26,4 +19,15 @@ function showError(err) {
   throw new Error(chalk.red(err))
 }
 
+function extractLinks(text) {
+  const regex = /\[([^]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
+  const extractedLinks = text.match(regex)
+  console.log(extractedLinks)
+}
+
 getFile(filePath)
+extractLinks(getFile(filePath))
+
+// \[[^]]*\]
+// \(https?:\/\/[^$#\s].[^\s]*\)
+// \[([^]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)
